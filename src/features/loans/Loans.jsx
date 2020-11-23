06e6525727d632e4ea3bf9d9765bc1f0b5c1c61b
Zdenek Bidrman <31826605+Bidrman@users.noreply.github.com>
2020-11-23 13:11:24 +0100

    css

--- src/features/loans/Loans.jsx
+++ src/features/loans/Loans.jsx
@@ -13,7 +13,7 @@ const Loans = () => {
         <div className={'loans'}>
             <Row className={'mt-2 mt-md-4 d-flex flex-row justify-content-center justify-content-md-around'}>
                 <CustomCard
-                    className={'biggerCard mt-1 mb-4'}
+                    className={'biggerCard mt-1 mb-4 mb-lg-5'}
                     backgroundImage
                     title={'půjčky'}
                     text={'S extra nízkým úrokem, právě pro vás. Dělejte to, co máte rádi'}
@@ -21,7 +21,7 @@ const Loans = () => {
                     icon={<HandLend fill={'#d30010'} width={'50'} height={'45'} />}
                 />
                 <CustomCard
-                    className={'biggerCard mt-1 mb-4'}
+                    className={'biggerCard mt-1 mb-4 mb-lg-5'}
                     backgroundImage
                     title={'kreditní karta'}
                     text={'Mějte peníze stále k dispozici. Navíc karta pomůže v nouzi.'}
@@ -31,22 +31,22 @@ const Loans = () => {
             </Row>
             <Row className={'d-flex flex-row justify-content-center justify-content-md-around'}>
                 <CustomCard
-                    className={'smallerCard'}
+                    className={'smallerCard mb-3'}
                     title={'konsolidace'}
                     text={'Snižte si splátky, ušetřete a kupte si něco hezkého na sebe'}
                     buttonText={'více o konsolidaci'}
                     icon={<Atom fill={'#d30010'} width={'42'} height={'42'} />}
                 />
                 <CustomCard
-                    className={'smallerCard'}
-                    title={'konsolidace'}
+                    className={'smallerCard mb-3'}
+                    title={'nákup na splátky'}
                     text={'Snižte si splátky, ušetřete a kupte si něco hezkého na sebe'}
                     buttonText={'více o konsolidaci'}
                     icon={<ShoppingBag fill={'#d30010'} width={'42'} height={'42'} />}
                 />{' '}
                 <CustomCard
-                    className={'smallerCard'}
-                    title={'konsolidace'}
+                    className={'smallerCard mb-3'}
+                    title={'financování auta'}
                     text={'Snižte si splátky, ušetřete a kupte si něco hezkého na sebe'}
                     buttonText={'více o konsolidaci'}
                     icon={<Car fill={'#d30010'} width={'42'} height={'42'} />}
