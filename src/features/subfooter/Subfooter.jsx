02452246f00cd9fb3a3d1a2beb98169047d7aaf1
Zdenek Bidrman <31826605+Bidrman@users.noreply.github.com>
2020-11-25 13:21:52 +0100

    last upravy

--- src/features/subfooter/Subfooter.jsx
+++ src/features/subfooter/Subfooter.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Row, Col, Container } from 'reactstrap'
+import { Col, Container } from 'reactstrap'
 import Navbar from 'react-bootstrap/Navbar'
 
-import { Envelope, Telephone, GeoAlt } from 'react-bootstrap-icons'
+import LinkedIn from '../../components/icons/linkedin'
+import Facebook from '../../components/icons/fb'
+import Youtube from '../../components/icons/youtube'
 
 const Subfooter = () => {
     return (
@@ -10,11 +12,11 @@ const Subfooter = () => {
             <Navbar expand={'lg'} collapseOnSelect className={'w-100 subfooter__navigation'}>
                 <Col>
                     <Navbar.Toggle aria-controls='footerAirbank'>
-                        <h4 className='text-uppercase font-weight-bold text-black mb-3 font-20'>
+                        <h4 className='text-uppercase font-weight-bold text-black mb-3 font-16 ls-1'>
                             Naše produkty
                         </h4>
                     </Navbar.Toggle>
-                    <h4 className='text-uppercase font-weight-bold text-black mb-3 ghost  font-20'>
+                    <h4 className='text-uppercase font-weight-bold text-black mb-3 ghost font-16 ls-1'>
                         Naše produkty
                     </h4>
                     <Navbar.Collapse id={'footerAirbank'}>
@@ -49,11 +51,11 @@ const Subfooter = () => {
                 </Col>
                 <Col>
                     <Navbar.Toggle aria-controls='footerOffer'>
-                        <h4 className='text-uppercase font-weight-bold text-black mb-3  font-20'>
+                        <h4 className='text-uppercase font-weight-bold text-black mb-3  font-16 ls-1'>
                             O home credit
                         </h4>
                     </Navbar.Toggle>
-                    <h4 className='text-uppercase font-weight-bold text-black mb-3 ghost font-20'>
+                    <h4 className='text-uppercase font-weight-bold text-black mb-3 ghost font-16 ls-1'>
                         O home credit
                     </h4>
                     <Navbar.Collapse id={'footerOffer'}>
@@ -88,14 +90,21 @@ const Subfooter = () => {
                 </Col>
                 <Col>
                     <Navbar.Toggle aria-controls='footerSupport'>
-                        <h4 className='text-uppercase font-weight-bold text-black mb-3 font-20'>Podpora</h4>
+                        <h4 className='text-uppercase font-weight-bold text-black mb-3 font-16 ls-1'>
+                            Podpora
+                        </h4>
                     </Navbar.Toggle>
-                    <h4 className='text-uppercase font-weight-bold text-black mb-3 ghost font-20'>Podpora</h4>
-                    <Navbar.Collapse id={'footerSupport'}>
+                    <h4 className='text-uppercase font-weight-bold text-black mb-3 ghost font-16 ls-1'>
+                        Podpora
+                    </h4>
+                    <Navbar.Collapse id={'footerSupport'} className={'d-flex flex-column'}>
+                        <div className={'phone-number d-flex flex-column'}>
+                            <a className={''} href='#'>
+                                +&nbsp;420&nbsp;542&nbsp;100&nbsp;100
+                            </a>
+                            <span className=''>V pracovní dny od 8 do 18 hodin</span>
+                        </div>
                         <ul className='subfooter__list'>
-                            <li className='subfooter__list-item mb-2'>
-                                <a href='#' className='subfooter__link'></a>
-                            </li>
                             <li className='subfooter__list-item mb-2'>
                                 <a href='#' className='subfooter__link'>
                                     Nejdůležitější dotazy
@@ -110,13 +119,23 @@ const Subfooter = () => {
                     </Navbar.Collapse>
                 </Col>
                 <Col>
-                    <h4 className='text-uppercase font-weight-bold text-black mb-3  font-20'>
+                    <h4 className='text-uppercase font-weight-bold text-black mb-3  font-16 ls-1'>
                         Sociální sítě
                     </h4>
-                    <div className='mb-4'></div>
-                    <div className='mb-3'></div>
-                    <div className='mb-3'></div>
-                    <div className='mb-3'></div>
+                    <div className='mb-3 d-flex flex-row align-items-center'>
+                        <div className='icon mr-2'>
+                            <Facebook width='24' height='24' fill='#404040' />
+                        </div>
+                        <div className='icon mr-2'>
+                            <Youtube width='24' height='24' fill='#404040' />
+                        </div>
+                        <div className='icon mr-2'>
+                            <Facebook width='24' height='24' fill='#404040' />
+                        </div>
+                        <div className='icon mr-2'>
+                            <LinkedIn width='24' height='24' fill='#404040' />
+                        </div>
+                    </div>
                 </Col>
             </Navbar>
         </Container>
